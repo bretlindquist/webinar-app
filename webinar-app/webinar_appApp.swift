941f989a7fedc2c887e598edab7b124821978722
Bret Lindquist <30852056+bretlindquist@users.noreply.github.com>
2024-03-01 07:58:55 +0900

    Initial Commit

---
+++ webinar-app/webinar_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  webinar_appApp.swift
+//  webinar-app
+//
+//  Created by Bret Allan Lindquist on 3/1/24.
+//
+
+import SwiftUI
+
+@main
+struct webinar_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
